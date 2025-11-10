0a6a244102cf9def180f92c2023df0529843f328
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 15:32:56 +0530

    Create temp_local.py

---
+++ temp_local.py
@@ -0,0 +1,25 @@
+from picarx import Picarx   # your hardware file (the big class you pasted)
+import time
+
+speed = 25
+px = Picarx()
+
+actions = ["forward", "left", "right", "reverse"]
+
+while actions:  # runs while the list is not empty
+    current_action = actions.pop(0)  # remove the first item
+    if current_action == "forward":
+        px.forward(speed)
+    elif current_action == "left":
+        for angle in range(0,35,10):
+            px.set_dir_servo_angle(angle)
+        px.forward(speed)
+    elif current_action == "right":
+        for angle in range(0,35,10):
+            px.set_dir_servo_angle(angle)
+        px.forward(speed)
+    elif current_action == "reverse":
+        px.forward(-25)
+    time.sleep(5)
+
+print("All actions completed!")
