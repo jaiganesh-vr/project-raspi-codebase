f9ccc37239e37843fec8895cd30914c684c82e36
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 21:23:26 +0530

    Update temp_local.py

--- temp_local.py
+++ temp_local.py
@@ -35,7 +35,7 @@ def move_left(speed):
         px.set_dir_servo_angle(angle)
         time.sleep(0.075)
     px.forward(speed)
-    time.sleep(2)
+    time.sleep(2.25)
     px.stop()
     for angle in range(-50, 0, 5):
         px.set_dir_servo_angle(angle)
