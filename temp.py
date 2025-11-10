44bc4e6cabb922e40ea8d3d25f0604d5b0a5698f
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 14:12:44 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -6,16 +6,24 @@ if __name__ == "__main__":
     try:
         # init picarx
         px = Picarx()
-
-        for angle in range(0, 35, 5):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.5)
-        for angle in range(35, -35, 5):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.5)
-        for angle in range(-35, 0, 5):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.5)
+        for angle in range(0, 35):
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35, -35, -1):
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35, 0):
+            px.set_cam_pan_angle(angle)
+            time.sleep(0.01)
+        for angle in range(0, 35):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35, -35,-1):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35, 0):
+            px.set_cam_tilt_angle(angle)
+            time.sleep(0.01)
 
     finally:
         px.stop()
