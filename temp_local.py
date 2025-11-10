0a3692de9c9297d000059ddf26c3d0bfdb73c1be
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 20:46:41 +0530

    Update temp_local.py

---
+++ temp_local.py
@@ -0,0 +1,41 @@
+from picarx import Picarx
+import time
+
+speed = 0.001
+actions = ["forward", "reverse"]
+
+px = Picarx()
+
+def move_forward(speed):
+    px.forward(speed)
+    time.sleep(1)
+    px.stop
+
+def move_reverse(speed):
+    for angle in range(0, 40, 5):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.075)
+    px.forward(speed)
+    time.sleep(1)
+    for angle in range(40, 0, -5):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.075)
+    px.stop 
+
+if __name__ == "__main__":
+    try:
+        while actions:
+            current_actions = actions.pop(0)
+            if current_actions == "forward":
+                move_forward(speed)
+            elif current_actions == "reverse":
+                move_reverse(speed)
+            elif current_actions == "right":
+                move_right(speed)
+            elif current_actions == "left":
+                move_left(speed)
+    finally:
+        px.stop()
+        time.sleep(0.2)
+
+
