3871149b88388dff102d8db2f20219502d574162
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 14:02:29 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,10 +7,25 @@ if __name__ == "__main__":
         # init picarx
         px = Picarx()
 
-        for x in range(-100,0,10):
+        for x in range(0,100,10):
             px.forward(x)
             print(x)
-            time.sleep(10)
+            time.sleep(2)
+
+        for x in range(0,-100,10):
+            px.forward(x)
+            print(x)
+            time.sleep(2)
+
+        for angle in range(0, 35, 2.5):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35, -35, -1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+        for angle in range(-35, 0, 2.5):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
 
     finally:
         px.stop()
