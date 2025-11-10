33e2d13181ac31d5ec96fbcc6be11807bae360b0
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 14:06:25 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
 
         for angle in range(0, 35, 2):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(1)
         for angle in range(35, -35, -1):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(1)
         for angle in range(-35, 0, 2):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+            time.sleep(1)
 
     finally:
         px.stop()
