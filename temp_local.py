4ca176eb31622ea5bde59fe3f225b50f998f9cde
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-10 16:08:57 +0530

    Update temp_local.py

--- temp_local.py
+++ temp_local.py
@@ -1,8 +1,13 @@
-from picarx import Picarx   # your hardware file (the big class you pasted)
+from robot_hat.utils import reset_mcu
+from picarx import Picarx  
 import time
+
+reset_mcu()
+time.sleep(0.2)
 
 i = 1
 speed = 25
+
 px = Picarx()
 
 actions = ["forward", "left", "right", "reverse", "stop"]
@@ -16,19 +21,26 @@ try:
             time.sleep(1)
             px.stop()
             i += 1
-        elif current_action == "left":
-            px.set_dir_servo_angle(-30)
+        elif current_action == "reverse":
+            px.forward(-25)
             time.sleep(1)
             px.stop()
             i += 1
         elif current_action == "right":
-            px.set_dir_servo_angle(30)
+            for angle in range(0,35,5):
+                 px.set_dir_servo_angle(angle)
             time.sleep(1)
             px.stop()
-            i += 1       
-        elif current_action == "reverse":
-            px.forward(-25)
-            time.sleep(1)
+            i += 1
+        elif current_action == "straight":
+            for angle in range(0,35,5):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.25)
+            i += 1
+        elif current_action == "left":
+            for angle in range(0,-35,5):
+                px.set_dir_servo_angle(angle)
+                time.sleep(0.25)
             i += 1
         elif current_action == "stop":
             px.stop()
