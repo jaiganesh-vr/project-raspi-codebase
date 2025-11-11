baee7032d8b8d5b575552b2058d91d6e957aea8f
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 14:25:12 +0530

    Update driver.py

--- automl/driver.py
+++ automl/driver.py
@@ -15,7 +15,7 @@ DRIVE_SPEED = 10        # Normal forward driving speed
 TURN_TIME_RIGHT = 1.6      # Seconds to complete a 90° turn
 TURN_TIME_LEFT = 1.6      # Seconds to complete a 90° turn
 TURN_TIME_180 = 3.2     # Seconds to complete a 180° turn
-PAUSE_BETWEEN_ACTIONS = 0  # Seconds to pause after each action
+PAUSE_BETWEEN_ACTIONS = 1  # Seconds to pause after each action
 
 # --- Movement Functions ---
 def move_forward(px, duration=1.0, speed=DRIVE_SPEED):
