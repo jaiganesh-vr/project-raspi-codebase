a9e60a9ff7a34899ef1332cab5c5c7c4e07b5959
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 14:34:04 +0530

    Update main.py

--- automl/main.py
+++ automl/main.py
@@ -2,7 +2,7 @@ from picarx import Picarx
 import driver
 
 px = Picarx()
-actions = ["left"]
+actions = ["right"]
 #actions = ["right","forward"]
 #actions = ["left", "right", "reverse","forward"]
 #actions = ["forward", "reverse", "right", "straight", "left", "straight", "stop"]
