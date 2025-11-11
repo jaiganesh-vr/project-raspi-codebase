f3d6e08f009aa21a8db564345fa9bf2ca0cf10ec
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 17:08:23 +0530

    Update main.py

--- automl/main.py
+++ automl/main.py
@@ -2,13 +2,8 @@ from picarx import Picarx
 import driver
 
 px = Picarx()
-#actions = ["forward","forward","forward","forward","forward","forward","forward","forward"]
-#actions = ["forward","right","forward","right","forward","right","forward",]
-actions = ["forward","left","forward","left","forward","left","forward"]
-#actions = ["right","forward"]
-#actions = ["left", "right", "reverse","forward"]
-#actions = ["forward", "reverse", "right", "straight", "left", "straight", "stop"]
 
+actions = ["forward"]
 
 if __name__ == "__main__":
     try:
