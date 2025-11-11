02fc4c3e7e174bef0269962b989bb7ae6f53a10c
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 06:04:09 +0530

    Update main.py

--- automl/main.py
+++ automl/main.py
@@ -14,7 +14,7 @@ px = Picarx()
 # --- Constants ---
 TURN_SPEED = 30         # Moderate speed for turning
 DRIVE_SPEED = 30        # Normal forward driving speed
-TURN_TIME_RIGHT = 1.5      # Seconds to complete a 90° turn
+TURN_TIME_RIGHT = 1.75      # Seconds to complete a 90° turn
 TURN_TIME_LEFT = 1.5      # Seconds to complete a 90° turn
 TURN_TIME_180 = 2.2     # Seconds to complete a 180° turn
 PAUSE_BETWEEN_ACTIONS = 1  # Seconds to pause after each action
