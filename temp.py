a1a4d87c2df5e998a5e74b9ecc239cd598645139
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 09:11:32 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -18,30 +18,20 @@ TURN_TIME_LEFT = 1.6      # Seconds to complete a 90° turn
 
 def turn_left(px, speed=TURN_SPEED):
     """Turn the car left by ~90 degrees."""
-    for angle in range(0, -20, -2):
+    for angle in range(0, -32, -2):
          px.set_dir_servo_angle(angle)
     time.sleep(0.5)
     px.set_motor_speed(1, 0)       # left wheel stopped
     px.set_motor_speed(2, -speed) 
     time.sleep(0.8)
     px.stop()
-    for angle in range(-26,-30, -2):
-         px.set_dir_servo_angle(-20)
-    time.sleep(0.5)    
     px.set_motor_speed(1, 0)       # left wheel stopped
     px.set_motor_speed(2, -speed) 
     time.sleep(0.8)
     px.stop()
-    for angle in range(-26,-32,-2):
-         px.set_dir_servo_angle(-30)
-    time.sleep(0.5)    
     px.set_motor_speed(1, 0)       # left wheel stopped
     px.set_motor_speed(2, -speed) 
     time.sleep(0.8)
-    px.stop()
-    for angle in range(-32, 2, 2):
-        px.set_dir_servo_angle(angle)
-        time.sleep(0.001)
 
 actions = ["left"]
 
