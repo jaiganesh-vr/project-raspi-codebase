2973c4fd3166fcd0a38eae4dcf25198c73c2919f
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 15:52:12 +0530

    Update driver.py

--- automl/driver.py
+++ automl/driver.py
@@ -45,7 +45,7 @@ def turn_right(px, speed=TURN_SPEED):
         time.sleep(0.110)
     px.set_motor_speed(1, speed)   # left wheel active
     px.set_motor_speed(2, 0)   
-    time.sleep(0.725)
+    time.sleep(0.825)
     px.stop()
     for angle in range(30, 0, -2):
         px.set_dir_servo_angle(angle)
