0323aa2267abb5762f556a872d288a0fedf1d4db
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 09:26:51 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -24,6 +24,7 @@ def turn_left(px, speed=TURN_SPEED):
     for angle in range(0, -35, -5):
         px.set_dir_servo_angle(angle)
         time.sleep(0.125)
+    time.sleep(0.8)    
     px.stop()
     for angle in range(-32, 2, 2):
         px.set_dir_servo_angle(angle)
