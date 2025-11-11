e9d9beb06b5a7edb4d712890ac674f148f70a5eb
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 10:18:18 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -49,8 +49,8 @@ def reverse(px, speed=TURN_SPEED):
     px.set_motor_speed(2, 0)   
     for angle in range(0, 35, 5):
         px.set_dir_servo_angle(angle)
-        time.sleep(0.125)
-    time.sleep(0.500)
+        time.sleep(0.250)
+    time.sleep(0.250)
     px.stop()
     for angle in range(30, 0, -2):
         px.set_dir_servo_angle(angle)
