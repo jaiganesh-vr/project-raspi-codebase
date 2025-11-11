8d49f0ccf06d89be33d3c5e62d41910a8f3035a4
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-11 10:14:34 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -20,7 +20,7 @@ def turn_left(px, speed=TURN_SPEED):
     """Turn the car left by ~90 degrees."""
 
     px.set_motor_speed(1, 0)       # left wheel stopped
-    px.set_motor_speed(2, -speed) 
+    px.set_motor_speed(2, -speed*2) 
     for angle in range(0, -35, -5):
         px.set_dir_servo_angle(angle)
         time.sleep(0.25)
