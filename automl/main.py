0eb747a918b9d6ecdb68dea4d4baee2ce84586a3
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-12 09:22:54 +0530

    Update main.py

--- automl/main.py
+++ automl/main.py
@@ -1,16 +1,49 @@
 from picarx import Picarx  
+import readchar
 import driver
+import time
 
-px = Picarx()
+manual = '''
+Select one of the following mode for Picar X :)
+    d: Drive
+    m: Manual
+    e: Explore
+    r: Race
+    ctrl+c: Quit
+'''
+def show_info():
+    print("\033[H\033[J",end='')  # clear terminal windows
+    print(manual)
 
 actions = ["left","right","reverse","forward"]
 
+
 if __name__ == "__main__":
+   
     try:
-        driver.drive(px,actions)
-    except Exception as e:    
-        print("error:%s"%e)
+        px = Picarx()
+        show_info()
+        while True:
+            key = readchar.readkey()
+            key = key.lower()
+            if key in('dmer'):
+                if 'd' == key:
+                    driver.drive(px,actions)
+                elif 'm' == key:
+                    driver.manual()
+                elif 'e' == key:
+                    px.set_dir_servo_angle(-35)
+                    px.forward(80)
+                elif 'r' == key:
+                    px.set_dir_servo_angle(35)
+                    px.forward(80)
+
+            elif key == readchar.key.CTRL_C:
+                print("\n Quit")
+                break
     finally:
+        px.set_cam_tilt_angle(0)
+        px.set_cam_pan_angle(0)
+        px.set_dir_servo_angle(0)
         px.stop()
-
-
+        sleep(.2)
