05f5a646d2ecb9e0c27e0bbc0c174864f15abd33
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-16 07:01:08 +0530

    Update main.py

--- automl/main.py
+++ automl/main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             key = key.lower()
             if key in('amer'):
                 if 'a' == key:
-                    actions = ["forward","forward","forward"]
+                    actions = ["generate"]
                     driver.auto(px,actions)
                 elif 'm' == key:
                     driver.manual(px)
