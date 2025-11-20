fef67195d2301b1c2b2b8cc152e1b91d8c16e9f5
Jaiganesh <jaigan.pdy@gmail.com>
2025-11-20 17:31:52 +0530

    Update driver.py

--- automl/driver.py
+++ automl/driver.py
@@ -157,7 +157,7 @@ def forward_left(px):
         px.set_dir_servo_angle(x)
         time.sleep(0.0525)   
 
-# --- Auto Mode Functions ---
+# --- Ultrasonic Functions ---
 def read_distance(px):
   safe_distance = 40
   danger_distance = 20
@@ -171,11 +171,45 @@ def read_distance(px):
             else:
                 print("not safe")
 
+# --- Camera Mode Functions ---
+
+def clamp_number(num,a,b):
+  return max(min(num, max(a, b)), min(a, b))
+
+def stare_at(px):
+    Vilib.camera_start()
+    Vilib.display()
+    Vilib.face_detect_switch(True)
+    x_angle =0
+    y_angle =0
+    while True:
+        if Vilib.detect_obj_parameter['human_n']!=0:
+            coordinate_x = Vilib.detect_obj_parameter['human_x']
+            coordinate_y = Vilib.detect_obj_parameter['human_y']
+            
+            # change the pan-tilt angle for track the object
+            x_angle +=(coordinate_x*10/640)-5
+            x_angle = clamp_number(x_angle,-35,35)
+            px.set_cam_pan_angle(x_angle)
+
+            y_angle -=(coordinate_y*10/480)-5
+            y_angle = clamp_number(y_angle,-35,35)
+            px.set_cam_tilt_angle(y_angle)
+
+            time.sleep(0.05)
+
+        else :
+            pass
+            time.sleep(0.05)
+
 # --- Auto Mode Functions ---
 
 def auto(px,actions):
     start = (3, 3) 
     facing = "up"
+    Vilib.camera_start()
+    Vilib.display()
+    Vilib.face_detect_switch(True)
     while actions:  # runs while the list is not empty
         distance = round(px.ultrasonic.read(), 2)
         if distance <= 20:
